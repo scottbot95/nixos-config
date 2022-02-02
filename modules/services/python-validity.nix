35e2d0411f3aec44afb6797e73bcd18365a6f3fb
Scott Techau <scotttechau@gmail.com>
2022-02-01 21:52:57 -0800

    Add service to restart python-validity after suspend
    This is a workaround for uunicorn/python-validity#106

--- modules/services/python-validity.nix
+++ modules/services/python-validity.nix
@@ -16,6 +16,19 @@ in with lib; {
     systemd = {
       packages = [ open-fprintd python-validity ];
       services.python3-validity.wantedBy = [ "default.target" ];
+
+      # Dirty hack to fix https://github.com/uunicorn/python-validity/issues/106
+      services.validity-restart = let
+        targets = [ "suspend.target" "hibernate.target" "hybrid-sleep.target" "suspend-then-hibernate.target" ];
+      in {
+        description = "Restart services to fix fingerprint integration";
+        wantedBy = targets;
+        after = targets;
+        serviceConfig = {
+          type = "oneshot";
+          ExecStart = "systemctl restart open-fprintd.service python3-validity.service";
+        };
+      };
     };
 
     services.dbus.packages = [ open-fprintd python-validity ];

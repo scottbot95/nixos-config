b6bf1e7cae3a63a9e61e1bdd18238469ba8d49e9
Scott Techau <scott.techau@gmail.com>
2022-02-03 16:08:37 -0800

    Delete some unneded comments

--- modules/services/yubikey.nix
+++ modules/services/yubikey.nix
@@ -10,14 +10,6 @@
     pinentry-qt
   ];
 
-  # Depending on the details of your configuration, this section might be necessary or not;
-  # feel free to experiment
-  # environment.shellInit = ''
-  #   export GPG_TTY="$(tty)"
-  #   gpg-connect-agent /bye
-  #   export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
-  # '';
-
   programs = {
     ssh.startAgent = false;
     gnupg.agent = {
--- systems/marvin/configuration.nix
+++ systems/marvin/configuration.nix
@@ -76,10 +76,7 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+
   programs.zsh = {
     enable = true;
     ohMyZsh.enable = true;
@@ -111,9 +108,6 @@
 
 
   # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];

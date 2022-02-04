83d1e1b61f8e98f1f3ee64a195a9f27de7e71ce0
Scott Techau <scott.techau@gmail.com>
2022-02-03 16:50:01 -0800

    Add some config to make yubikey work

--- modules/services/yubikey.nix
+++ modules/services/yubikey.nix
@@ -10,11 +10,23 @@
     pinentry-qt
   ];
 
+  # FIXME We shouldn't need this but for some reason gpgconf can't find
+  #       the ssh socket on its own so we have to help out
+  environment.shellInit = ''
+    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+  '';
+
   programs = {
     ssh.startAgent = false;
     gnupg.agent = {
       enable = true;
       enableSSHSupport = true;
+    };
+  };
+
+  home-manager.users.scott = {
+    programs.zsh.shellAliases = {
+      switch-yk = "gpg-connect-agent 'scd serialno' 'learn --force' /bye";
     };
   };
 }

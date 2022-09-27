71e6cab5b9fa177fc5827c59a69ae8d7d981a7f4
Scott Techau <scott.techau@gmail.com>
2022-09-26 22:23:50 -0700

    Patch flake nixops config to remove machines we can't build though CI

--- herculesCI.nix
+++ herculesCI.nix
@@ -14,17 +14,22 @@ in
             hercules-ci-effects.overlay
           ];
         };
+        excludeMachines = [ "bob-the-builder" "ns2" "public-ns" ];
       in {
         effects = {
           deploy-homelab = pkgs.effects.runIf /*(branch == "master")*/ true (
             pkgs.effects.runNixOps2 {
-              flake = self;
+              flake = self // {
+                nixopsConfigurations.default = 
+                  let 
+                    trimmedNetwork = builtins.removeAttrs self.nixopsConfigurations.default excludeMachines;
+                  in
+                    trimmedNetwork;
+              };
               prebuildOnlyNetworkFiles = [ ./networks/prebuild-stub.nix ];
               action = "dry-run";
-              extraDeployArgs = [
-                "--exclude" "bob-the-builder" "raspberrytau"
-              ];
               makeAnException = "I know this can corrupt the state, until https://github.com/NixOS/nixops/issues/1499 is resolved.";
+              extraDeployArgs = [ "--exclude" ] ++ excludeMachines;
             }
           );
         };

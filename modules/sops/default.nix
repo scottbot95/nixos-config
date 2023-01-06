43bb6c586b6dc875084e169cce89b56a74b0b3bc
Scott Techau <scott.techau@gmail.com>
2023-01-06 15:47:42 -0800

    Deploy sops key to non-ephermeral storage
    The sops key needs to be available during generation activation.
    This means, unlike many services which could automatically restart,
    running `nixops send-keys` will not help and only a `nixops deploy` will enable services to run.
    Also reboots will never work, even through `nixops reboot`

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -2,15 +2,16 @@
 with lib;
 let
   cfg = config.scott.sops;
+  users = config.users.users;
   envFilesType = types.submodule ({name, config, ...}: {
     options = {
       path = mkOption {
         type = types.str;
-        default = "/run/secrets/env/${name}";
-        defaultText = mdDoc "`/run/secrets/env/\${name}`";
+        default = "/run/secrets/${name}.env";
+        defaultText = literalExpression "/run/secrets/\${name}.env";
         description = "Path to store created secrets env file.";
       };
-      environment = mkOption {
+      vars = mkOption {
         type = with types; attrsOf str;
         description = mdDoc ''
           Attribute set of environment variables to include in the generated file.
@@ -19,6 +20,33 @@ let
         example = {
           FOO = "my/secret";
         };
+      };
+      mode = mkOption {
+        type = types.str;
+        default = "0400";
+        description = ''
+          Permissions mode of the in octal.
+        '';
+      };
+      owner = mkOption {
+        type = types.str;
+        default = "root";
+        description = ''
+          User of the file.
+        '';
+      };
+      group = mkOption {
+        type = types.str;
+        default = users.${config.owner}.group;
+        defaultText = literalExpression "config.users.users.\${owner}.group";
+        description = ''
+          Group of the file.
+        '';
+      };
+      requiredBy = mkOption {
+        type = with types; listOf str;
+        description = "List of systemd services that depend on this file existing";
+        default = [];
       };
     };
   });
@@ -46,24 +74,35 @@ in {
         sops.age.keyFile = cfg.ageKeyFile;
 
         systemd.services = mapAttrs'
-          (name: value: 
+          (name: envFile: 
             nameValuePair "make-env-${name}" {
               description = "Create environment file '${name}'";
 
               wantedBy = [ "multi-user.target" ];
+              requiredBy = envFile.requiredBy;
 
               restartIfChanged = true;
 
               serviceConfig = {
                 Type = "oneshot";
-                ExecStart = pkgs.writeShellScript "make-env-${name}" (
-                  concatStringsSep "\n" (
+                ExecStart = let
+                  readVars = concatStringsSep "\n" (
                     mapAttrsToList (varName: secretName: ''
                       ${varName}=$(cat ${config.sops.secrets.${secretName}.path})
-                    '') value 
-                  )
-                );
-                DynamicUser = true;
+                    '') envFile.vars 
+                  );
+                  writeVars = concatStringsSep "\n" (
+                    map (name: "${name}=\$${name}") (builtins.attrNames envFile.vars)
+                  );
+                in pkgs.writeShellScript "make-env-${name}" ''
+                  ${readVars}
+                  cat <<EOT > ${envFile.path}
+                  ${writeVars}
+                  EOT
+
+                  chmod ${envFile.mode} ${envFile.path}
+                  chown ${envFile.owner}:${envFile.group} ${envFile.path}
+                '';
               };
             }
           )
--- networks/modules/proxmoxGuest/sops.nix
+++ networks/modules/proxmoxGuest/sops.nix
@@ -1,11 +1,14 @@
 { config, lib, pkgs, ...}: {
   config = lib.mkIf config.scott.sops.enable {
-    scott.sops.ageKeyFile = "/run/keys/sops-age-key";
+    scott.sops.ageKeyFile = "/var/lib/.secrets/sops-age-key";
 
-    deployment.keys.sops-age-key.keyCommand = [
-      "${pkgs.sops}/bin/sops" 
-      "--extract" ''["sops_key"]''
-      "-d" "secrets/homelab.yaml"
-    ];
+    deployment.keys.sops-age-key = {
+      destDir = "/var/lib/.secrets";
+      keyCommand = [
+        "${pkgs.sops}/bin/sops" 
+        "--extract" ''["sops_key"]''
+        "-d" "secrets/homelab.yaml"
+      ];
+    };
   };
 }

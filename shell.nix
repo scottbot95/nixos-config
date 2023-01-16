ec34a6c0abf3fb5559aa665569919cc5c4c290b0
Scott Techau <scott.techau@gmail.com>
2023-01-16 09:26:25 -0800

    Fix some issues with nix deployment

--- shell.nix
+++ shell.nix
@@ -1,25 +1,20 @@
 { pkgs ? import <nixpkgs> {},
   system ? pkgs.system,
-  flake ? null,
+  flake ? builtins.getFlake (toString ./.),
 }:
 let
-  safeFlake = 
-    if flake != null then 
-      flake
-    else
-      builtins.getFlake (toString ./.);
   system = pkgs.system;
 in 
 with pkgs;
 mkShell {
     nativeBuildInputs = [
-        safeFlake.inputs.hercules-ci-agent.packages.${system}.hercules-ci-cli
+        flake.inputs.hercules-ci-agent.packages.${system}.hercules-ci-cli
     ];
     buildInputs = [
         cachix
         sops
         fly
-        safeFlake.packages.${system}.nixops
-        safeFlake.inputs.nixops-proxmox.packages.${system}.default
+        flake.packages.${system}.nixops
+        flake.inputs.nixops-proxmox.packages.${system}.default
     ];
 }

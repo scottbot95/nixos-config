c53e1ab17b6333e5cdf8a31c567bab0a3173d849
Scott Techau <scott.techau@gmail.com>
2024-02-10 12:05:25 -0800

    Configure palworld server

--- machines/steam/configuration.nix
+++ machines/steam/configuration.nix
@@ -13,12 +13,49 @@ in
   };
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "7-days-to-die-server"
     "steamworks-sdk-redist"
+    "palworld-server"
+    "steam-run"
+    "steam-original"
   ];
 
+  sops.defaultSopsFile = ./secrets.yaml;
+  scott.sops.enable = true;
+  sops.secrets."palworld/mesa/gamePass" = {};
+  sops.secrets."palworld/mesa/adminPass" = {};
+
+  scott.sops.envFiles.palworld-mesa = {
+    requiredBy = [ "palworld-mesa.service" ];
+    vars = {
+      SERVER_PASSWORD.secret = "palworld/mesa/gamePass";
+      ADMIN_PASSWORD.secret = "palworld/mesa/adminPass";
+    };
+  };
+
   services.steam-servers."7-days-to-die".drazz = {
+    enable = false;
+    openFirewall = true;
+
+    config = {
+      GameWorld = "PREGEN10k";
+      GameName = "GoeffPlsNoGrief";
+    };
+  };
+
+  services.steam-servers.palworld.mesa = {
     enable = true;
     openFirewall = true;
+
+    worldSettings = {
+      ServerPassword = "@SERVER_PASSWORD@";
+      AdminPassword = "@ADMIN_PASSWORD@";
+      PalEggDefaultHatchingTime = 25;
+    };
+  };
+
+  systemd.services.palworld-mesa = {
+    serviceConfig.EnvironmentFile = "/run/secrets/palworld-mesa.env";
   };
 
   networking.domain = "prod.faultymuse.com";
